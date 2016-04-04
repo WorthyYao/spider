bd3c93b7bd026d427ca253c1ca01fd20cbfb4af5
WorthyYao <worthyyao@163.com>
2016-04-04 16:29:05 +0800

    change

---
+++ dao/settings.js
@@ -0,0 +1,5 @@
+module.exports ={
+	cookieSecret:'worthy',
+	db:'spider',
+	host:'localhost',
+};
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,16 @@
 var express=require('express');
+var crypto=require('crypto');
+var user=require('../dao/user');
 var router=express.Router();
 
 router.get('/',function(req,res,next){
 	res.render("register",{title:'User register'});
+})
+
+router.post('/',function(req,res,next){
+	var md5=crypto.createHash('md5');
+	var password=md5.update(req.body.password).digest('base64')
+	console.log(password);
 })
 
 module.exports=router;
